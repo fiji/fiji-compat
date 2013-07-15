psteinb/fiji

Make sure that ImageJ2's legacy layer gets initialized
... before we start up ImageJ 1.x. This is needed for the upcoming
Help>Switch To Modern Mode.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

src-plugins/fiji-compat/src/main/java/fiji/IJ1Patcher.java
@@ -1,5 +1,11 @@
 package fiji;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import imagej.legacy.DefaultLegacyService;
+
 /**
  * Patch ij.jar using Javassist, handle headless mode, too.
  *
@@ -14,6 +20,14 @@ public class IJ1Patcher implements Runnable {
 			if ("true".equalsIgnoreCase(headless))
 				new Headless().run();
 			new IJHacker().run();
+			try {
+				// make sure that ImageJ2's LegacyInjector runs
+				DefaultLegacyService.getInstance();
+			} catch (final NoClassDefFoundError e) {
+				e.printStackTrace();
+				// ImageJ2 not installed
+				System.err.println("Did not find DefaultLegacyService class: " + e);
+			}
 			try {
 				JavassistHelper.defineClasses();
 			} catch (Exception e) {

fiji/fiji-compat

src/main/java/fiji/IJ1Patcher.java
@@ -1,5 +1,11 @@
 package fiji;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import imagej.legacy.DefaultLegacyService;
+
 /**
  * Patch ij.jar using Javassist, handle headless mode, too.
  *
@@ -18,6 +24,14 @@ public class IJ1Patcher implements Runnable {
 			if ("true".equalsIgnoreCase(headless))
 				new Headless().run();
 			new IJHacker().run();
+			try {
+				// make sure that ImageJ2's LegacyInjector runs
+				DefaultLegacyService.preinit();
+			} catch (final NoClassDefFoundError e) {
+				e.printStackTrace();
+				// ImageJ2 not installed
+				System.err.println("Did not find DefaultLegacyService class: " + e);
+			}
 			try {
 				JavassistHelper.defineClasses();
 			} catch (Exception e) {